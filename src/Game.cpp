edd2d3adc2c7ee40c9098faada0f6d145b404153
Manuel Garcia <alexgarcia2k@gmail.com>
2021-12-28 18:09:56 -0800

    Create SDL window

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,5 +1,6 @@
 #include "Game.h"
 #include <iostream>
+#include <SDL2/SDL.h>
 
 Game::Game() {
     std::cout << "Creating game object" <<std::endl;
@@ -7,7 +8,30 @@ Game::Game() {
 Game::~Game() {
     std::cout << "Destroying game" << std::endl;
 }
-void Game::Initialize(){}
+void Game::Initialize() {
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0 ) {
+        std::cerr << "Error initializing SDL " << std::endl;
+        return;
+    }
+    SDL_Window* window = SDL_CreateWindow(NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_BORDERLESS
+        );
+    if (!window) {
+        std::cerr << "Error creating SDL window " << std::endl;
+        return;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window,-1,0);
+
+    if(!renderer) {
+        std::cerr << "Error creating renderer " << std::endl;
+        return;
+    }
+}
 void Game::Run(){}
 void Game::ProcessInput(){}
 void Game::Update(){}
