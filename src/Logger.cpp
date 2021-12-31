f595eac1ea2311d89a624eb734e04862ce3f670c
Manuel Garcia <alexgarcia2k@gmail.com>
2021-12-31 05:42:32 -0800

    Full logger implementation

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,18 +1,50 @@
 #include "Logger.h"
 #include <iostream>
 #include <ctime>
-#include <iomanip>
+#include <chrono>
 
-void Logger::printWithCurrentTimestamp(const std::string& msg,const std::string& prefix,int color) {
-    auto t = std::time(nullptr);
-    auto tm = *std::localtime(&t);
+std::vector<LogEntry> Logger::messages;
 
-    std::cout << "\033[1;"<< color <<"m"<< prefix <<": [ " << std::put_time(&tm, "%d-%b-%Y %H:%M:%S") 
-            << " ] "<< msg << "\033[0m" <<std::endl;
+void Logger::printWithCurrentTimestamp(const std::string& msg,LogType type) {
+    std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::string prefix ;
+    int color;
+    switch (type)
+    {
+    case LOG_INFO:
+        prefix = "LOG";
+        color = 32;
+        break;
+    case LOG_ERROR:
+        prefix = "ERR";
+        color = 31;
+        break;
+    case LOG_WARNING:
+        prefix = "WARNING";
+        color = 33;
+        break;
+
+    default:
+        break;
+    }
+
+    std::string timeOut(30,'\0');
+    std::strftime (&timeOut[0],timeOut.size(),"%d-%b-%Y %H:%M:%S",std::localtime(&now));
+    LogEntry logEntry;
+    logEntry.type = LOG_INFO;
+    logEntry.message = prefix+": [ "+timeOut+" ] " + msg;
+
+    std::cout << "\033[1;"<< color <<"m"<< logEntry.message << "\033[0m" <<std::endl;
+
+    messages.push_back(logEntry);
+
 }
 void Logger::Log(const std::string& msg) {
-    printWithCurrentTimestamp(msg,"LOG",32);
+    printWithCurrentTimestamp(msg,LOG_INFO);
 }
 void Logger::Err(const std::string& msg) {
-    printWithCurrentTimestamp(msg,"ERR",31);
+    printWithCurrentTimestamp(msg,LOG_ERROR);
+}
+void Logger::Warn(const std::string& msg) {
+    printWithCurrentTimestamp(msg,LOG_WARNING);
 }
--- src/Logger.h
+++ src/Logger.h
@@ -1,13 +1,26 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 #include <string>
+#include <vector>
+
+enum LogType {
+    LOG_INFO,
+    LOG_WARNING,
+    LOG_ERROR
+};
+struct LogEntry {
+    LogType type;
+    std::string message;
+};
 
 class Logger {
     private:
-        static void printWithCurrentTimestamp(const std::string& msg,const std::string& prefix,int color);
+        static void printWithCurrentTimestamp(const std::string& msg,LogType type);
     public:
+        static std::vector<LogEntry> messages;
         static void Log(const std::string& msg);
         static void Err(const std::string& msg);
+        static void Warn(const std::string& msg);
 
 };
 
