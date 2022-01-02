5ba23e3fec3f61f5b18c12e5ec79c30b1e270719
Manuel Garcia <alexgarcia2k@gmail.com>
2022-01-02 05:45:30 -0800

    Component type template

--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -1,19 +1,73 @@
 #ifndef ECS_H
 #define ECS_H
 
-class Component {
+#include <bitset>
+#include <vector>
 
+const unsigned int MAX_COMPONENTS = 32;
+
+///////////////////////////////////////////////////////////////////////////
+// Signature
+///////////////////////////////////////////////////////////////////////////
+// We use a bitset to track which components an entity has and also helps
+// track which entities a system is interested in
+///////////////////////////////////////////////////////////////////////////
+
+typedef std::bitset<MAX_COMPONENTS> Signature;
+
+struct IComponent {
+    protected:
+        static int nextId;
+};
+
+// Used to assign a unique id to component type
+template <typename T>
+class Component : public IComponent {
+    // returns unique id of Component<T>
+    static int GetId () {
+        static auto id = nextId++;
+        return id;
+    }
 };
 
 class Entity {
-
+    private:
+        int id;
+    public:
+        Entity(int id): id(id) {};
+        int GetId() const;
 };
 
-class System {
+///////////////////////////////////////////////////////////////////////////
+// System
+///////////////////////////////////////////////////////////////////////////
+// This system processes entities that contain a specific signature
+///////////////////////////////////////////////////////////////////////////
 
+class System {
+    private:
+        Signature componentSignature ;
+        std::vector<Entity> entities;
+    public:
+        System() = default;
+        ~System() = default;
+        void AddEntityToSystem(Entity entity);
+        void RemoveEntityFromSystem(Entity entity);
+        std::vector<Entity> GetSystemEntities() const;
+        Signature& GetComponentSignature() const;
+
+        // Defines the component type that entities must have to be considered
+        // by the system
+        template <typename T> void RequireComponent();
 };
 
 class Registry {
 
 };
+
+template <typename T>
+void System::RequireComponent() {
+    const auto componentId = Component<T>::GetId();
+    componentSignature.set(componentId);
+}
 #endif
