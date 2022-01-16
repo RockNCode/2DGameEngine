6c29a9252f2d62e3bdafccf996a2b59e194fbaf6
Manuel Garcia <alexgarcia2k@gmail.com>
2022-01-16 15:35:30 -0800

    Displaying textures in render system

--- src/Components/SpriteComponent.h
+++ src/Components/SpriteComponent.h
@@ -1,16 +1,23 @@
 #ifndef SPRITECOMPONENT_H
 #define SPRITECOMPONENT_H
 #include <string>
+#include <SDL2/SDL.h>
 
 struct SpriteComponent {
     std::string assetId;
     int width;
     int height;
-
-    SpriteComponent(std::string assetId="", int width = 0, int height=0) {
+    SDL_Rect srcRect;
+    SpriteComponent(std::string assetId="", int width = 0, int height=0, int srcRectX =0 , int srcRectY=0) {
         this->assetId = assetId;
         this->width = width;
         this->height = height;
+        this->srcRect = {
+            srcRectX,
+            srcRectY,
+            width,
+            height
+        };
     }
 };
 
